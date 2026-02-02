179e196fc3835508a7452cd2ae9eed46de51a420
Hidano <n.hidano@hidano-dev.com>
2026-02-03 02:14:14 +0900

    task P02-04

---
+++ FacialControl/Packages/com.hidano.facialcontrol/Runtime/Domain/Models/FacialState.cs
@@ -0,0 +1,11 @@
+namespace Hidano.FacialControl.Domain.Models
+{
+    /// <summary>
+    /// 現在のアクティブ Expression 情報を保持する構造体。
+    /// IBlinkTrigger の引数として使用される。
+    /// 詳細な実装は P02-06 で行う。
+    /// </summary>
+    public readonly struct FacialState
+    {
+    }
+}
