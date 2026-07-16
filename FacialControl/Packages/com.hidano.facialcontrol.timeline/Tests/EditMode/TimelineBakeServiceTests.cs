c67e68b6e54b480b0d6fd014a91bbfe506d97046
Hidano <n.hidano@hidano-dev.com>
2026-07-17 06:37:44 +0900

    6.2 ベイクサービス（カーブ化・保存・決定性）を実装する
    TimelineBakeService に state event / analog / gaze のベイク生成を追加。
    TimelineBakeServiceTests を拡張し、state/value bake・決定性・IsStale を検証。
    rec-timeline-baking/tasks.md の 6.2 を完了に更新。

--- FacialControl/Packages/com.hidano.facialcontrol.timeline/Tests/EditMode/TimelineBakeServiceTests.cs
+++ FacialControl/Packages/com.hidano.facialcontrol.timeline/Tests/EditMode/TimelineBakeServiceTests.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Timeline;
 using Hidano.FacialControl.Timeline.Clips;
+using Hidano.FacialControl.Timeline.Domain.Models;
 using Hidano.FacialControl.Timeline.Domain.Services;
 using Hidano.FacialControl.Timeline.Tracks;
 
@@ -70,6 +71,174 @@ namespace Hidano.FacialControl.Timeline.Tests.EditMode
                     float actual = curve.Curve.Evaluate(time);
                     Assert.That(actual, Is.EqualTo(expected).Within(0.0001f), $"t={time}");
                 }
+            }
+            finally
+            {
+                if (bake != null)
+                {
+                    UnityEngine.Object.DestroyImmediate(bake);
+                }
+
+                UnityEngine.Object.DestroyImmediate(timeline);
+            }
+        }
+
+        [Test]
+        public void Bake_IncludesStateEventsAndMergedValueChannels()
+        {
+            var timeline = ScriptableObject.CreateInstance<TimelineAsset>();
+            FacialTimelineBakeAsset bake = null;
+
+            try
+            {
+                FacialProfile profile = CreateProfile(transitionDuration: 0.25f);
+                FacialExpressionTrack expressionTrack = CreateExpressionTrack(timeline, start: 0.025d, duration: 0.50d);
+                expressionTrack.name = "Expressions";
+
+                FacialValueTrack analogTrack = timeline.CreateTrack<FacialValueTrack>(null, "Analog");
+                analogTrack.ChannelSubId = "analog-main";
+                analogTrack.ChannelKind = FacialValueChannelKind.Analog;
+                TimelineClip analogClip = analogTrack.CreateClip<FacialValueClip>();
+                analogClip.start = 0.10d;
+                analogClip.duration = 0.30d;
+                ((FacialValueClip)analogClip.asset).Axes = new[]
+                {
+                    AnimationCurve.Linear(0f, 0f, 0.30f, 0.6f),
+                    AnimationCurve.Linear(0f, 1f, 0.30f, -1f),
+                };
+
+                FacialValueTrack gazeTrack = timeline.CreateTrack<FacialValueTrack>(null, "Gaze");
+                gazeTrack.ChannelSubId = "gaze-main";
+                gazeTrack.ChannelKind = FacialValueChannelKind.Gaze;
+                TimelineClip gazeClip = gazeTrack.CreateClip<FacialValueClip>();
+                gazeClip.start = 0.20d;
+                gazeClip.duration = 0.25d;
+                ((FacialValueClip)gazeClip.asset).Axes = new[]
+                {
+                    AnimationCurve.Linear(0f, -1f, 0.25f, 1f),
+                    AnimationCurve.Linear(0f, 1f, 0.25f, -1f),
+                };
+
+                bake = Editor.TimelineBakeService.Bake(timeline, profile);
+
+                Assert.That(bake.StateEvents, Has.Length.EqualTo(2));
+                Assert.That(bake.StateEvents[0].TimeSeconds, Is.EqualTo(0.025d).Within(1e-9d));
+                Assert.That(bake.StateEvents[0].ExpressionId, Is.EqualTo("smile"));
+                Assert.That(bake.StateEvents[1].TimeSeconds, Is.EqualTo(0.525d).Within(1e-9d));
+                Assert.That(bake.StateEvents[1].Kind, Is.EqualTo(TimelineStateEvent.KindOff));
+
+                Assert.That(bake.ValueBakes, Has.Length.EqualTo(2));
+
+                ValueChannelBake analogBake = FindValueBake(bake, "analog-main");
+                Assert.That(analogBake.IsGaze, Is.False);
+                Assert.That(analogBake.Axes, Has.Length.EqualTo(2));
+                Assert.That(analogBake.Axes[0].Evaluate(0.05f), Is.EqualTo(0f).Within(0.0001f));
+                Assert.That(analogBake.Axes[0].Evaluate(0.25f), Is.EqualTo(0.3f).Within(0.02f));
+                Assert.That(analogBake.Axes[0].Evaluate(0.45f), Is.EqualTo(0f).Within(0.0001f));
+
+                ValueChannelBake gazeBake = FindValueBake(bake, "gaze-main");
+                Assert.That(gazeBake.IsGaze, Is.True);
+                Assert.That(gazeBake.Axes, Has.Length.EqualTo(2));
+                Assert.That(gazeBake.Axes[0].Evaluate(0.20f), Is.EqualTo(-1f).Within(0.0001f));
+                Assert.That(gazeBake.Axes[0].Evaluate(0.325f), Is.EqualTo(0f).Within(0.02f));
+                Assert.That(gazeBake.Axes[0].Evaluate(0.46f), Is.EqualTo(0f).Within(0.0001f));
+            }
+            finally
+            {
+                if (bake != null)
+                {
+                    UnityEngine.Object.DestroyImmediate(bake);
+                }
+
+                UnityEngine.Object.DestroyImmediate(timeline);
+            }
+        }
+
+        [Test]
+        public void Bake_SameInputTwice_ProducesDeterministicBakeData()
+        {
+            var timeline = ScriptableObject.CreateInstance<TimelineAsset>();
+            FacialTimelineBakeAsset firstBake = null;
+            FacialTimelineBakeAsset secondBake = null;
+
+            try
+            {
+                FacialProfile profile = CreateProfile(transitionDuration: 0.25f);
+                CreateExpressionTrack(timeline, start: 0.025d, duration: 0.50d);
+
+                FacialValueTrack analogTrack = timeline.CreateTrack<FacialValueTrack>(null, "Analog");
+                analogTrack.ChannelSubId = "analog-main";
+                analogTrack.ChannelKind = FacialValueChannelKind.Analog;
+                TimelineClip analogClip = analogTrack.CreateClip<FacialValueClip>();
+                analogClip.start = 0.10d;
+                analogClip.duration = 0.20d;
+                ((FacialValueClip)analogClip.asset).Axes = new[]
+                {
+                    AnimationCurve.Linear(0f, 0f, 0.20f, 1f),
+                };
+
+                firstBake = Editor.TimelineBakeService.Bake(timeline, profile);
+                secondBake = Editor.TimelineBakeService.Bake(timeline, profile);
+
+                Assert.That(secondBake.SourceHashHex, Is.EqualTo(firstBake.SourceHashHex));
+                Assert.That(secondBake.StateEvents, Has.Length.EqualTo(firstBake.StateEvents.Length));
+                Assert.That(secondBake.ValueBakes, Has.Length.EqualTo(firstBake.ValueBakes.Length));
+
+                BlendShapeCurve firstCurve = FindCurve(firstBake, "Expressions", "Smile");
+                BlendShapeCurve secondCurve = FindCurve(secondBake, "Expressions", "Smile");
+                Assert.That(secondCurve.Curve.keys, Is.EqualTo(firstCurve.Curve.keys));
+
+                ValueChannelBake firstValueBake = FindValueBake(firstBake, "analog-main");
+                ValueChannelBake secondValueBake = FindValueBake(secondBake, "analog-main");
+                Assert.That(secondValueBake.Axes[0].keys, Is.EqualTo(firstValueBake.Axes[0].keys));
+            }
+            finally
+            {
+                if (firstBake != null)
+                {
+                    UnityEngine.Object.DestroyImmediate(firstBake);
+                }
+
+                if (secondBake != null)
+                {
+                    UnityEngine.Object.DestroyImmediate(secondBake);
+                }
+
+                UnityEngine.Object.DestroyImmediate(timeline);
+            }
+        }
+
+        [Test]
+        public void IsStale_WhenValueTrackChanges_ReturnsTrue()
+        {
+            var timeline = ScriptableObject.CreateInstance<TimelineAsset>();
+            FacialTimelineBakeAsset bake = null;
+
+            try
+            {
+                FacialProfile profile = CreateProfile(transitionDuration: 0.25f);
+                CreateExpressionTrack(timeline, start: 0.025d, duration: 0.50d);
+
+                FacialValueTrack valueTrack = timeline.CreateTrack<FacialValueTrack>(null, "Analog");
+                valueTrack.ChannelSubId = "analog-main";
+                valueTrack.ChannelKind = FacialValueChannelKind.Analog;
+                TimelineClip clip = valueTrack.CreateClip<FacialValueClip>();
+                clip.start = 0.0d;
+                clip.duration = 0.25d;
+                ((FacialValueClip)clip.asset).Axes = new[]
+                {
+                    AnimationCurve.Linear(0f, 0f, 0.25f, 0.5f),
+                };
+
+                bake = Editor.TimelineBakeService.Bake(timeline, profile);
+                Assert.That(Editor.TimelineBakeService.IsStale(timeline, profile, bake), Is.False);
+
+                ((FacialValueClip)clip.asset).Axes = new[]
+                {
+                    AnimationCurve.Linear(0f, 0f, 0.25f, 0.75f),
+                };
+
+                Assert.That(Editor.TimelineBakeService.IsStale(timeline, profile, bake), Is.True);
             }
             finally
             {
@@ -169,6 +338,23 @@ namespace Hidano.FacialControl.Timeline.Tests.EditMode
             }
 
             Assert.Fail($"Curve not found. layer={layerName}, blendShape={blendShapeName}");
+            return null;
+        }
+
+        private static ValueChannelBake FindValueBake(FacialTimelineBakeAsset bake, string sub)
+        {
+            Assert.That(bake, Is.Not.Null);
+            Assert.That(bake.ValueBakes, Is.Not.Null);
+
+            for (int i = 0; i < bake.ValueBakes.Length; i++)
+            {
+                if (string.Equals(bake.ValueBakes[i].Sub, sub, StringComparison.Ordinal))
+                {
+                    return bake.ValueBakes[i];
+                }
+            }
+
+            Assert.Fail($"Value bake not found. sub={sub}");
             return null;
         }
 
